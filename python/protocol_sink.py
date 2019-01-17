33077d3a093bc2c85515d4e9b99cffb11fff6b45
Blockstream Satellite <satellite@blockstream.com>
2019-01-16 22:02:52 -0300

    Fix protocol sink stopping due to a pipe overflow
    The previous version of the protocol sink block attempted to output data to the
    pipe in the same loop where data is received and processed. This created a
    problem when one of the pipes (for either API or blocks) was not being read by
    the user. In this scenario, eventually the pipe not being read would overflow
    such that a "pipe write" command to it would block. At this point, the reception
    of further GNU Radio PDUs bringing API or blocks data would also stop because
    the protocol sink module would be blocked on the pipe write. This means that the
    traffic of interest would stop due to the other traffic not being read.
    
    This patch separates reception of data from the outputting of data to the named
    pipe. Now the protocol sink block stores the data that comes via GNU Radio
    PDUs (containing Blocksat Packets) in independent local data buffers for API and
    blocks. Additionally, it launches two independent consumer threads, which wait
    for data to appear in these buffers and output the data to the corresponding
    named pipe when it becomes available. As a result, now if one of the two named
    pipes is not being read it is solely the corresponding thread that stops. The
    other thread keeps running, just like reception of GNU Radio PDUs from the
    upstream block continues to run.
    
    To complement this infrastructure, this patch introduces logic to observe and
    limit the occupancy of the internal buffers used for API and blocks. As the
    buffer gets close to becoming full, a warning is printed to the console. When it
    is full, another warning is printed. When this happens, old data in the buffer
    is thrown away such that the buffer doesn't exceed its maximum size.
    
    Lastly, this patch also adds option to disable reception and the output of
    blocks data. Hence, now both API and blocks data can be optionally disabled (API
    data could be disabled before).
    
    This patch relates to Blockstream/satellite Issue #26.

--- python/protocol_sink.py
+++ python/protocol_sink.py
@@ -22,6 +22,7 @@
 import numpy, time, math, struct
 import pmt
 from datetime import datetime
+from threading import Thread, Lock, Event
 from gnuradio import gr
 import blocksat
 from pipe import Pipe
@@ -37,6 +38,8 @@ OUT_DATA_HEADER_LEN        = 64 + 8
 OUT_DATA_DELIMITER         = 'vyqzbefrsnzqahgdkrsidzigxvrppato' + \
                              '\xe0\xe0$\x1a\xe4["\xb5Z\x0bv\x17\xa7\xa7\x9d' + \
                              '\xa5\xd6\x00W}M\xa6TO\xda7\xfaeu:\xac\xdc'
+MAX_BUFFER_SIZE            = 10 * 2 ** 20
+OVERFLOW_WARNING_POINT     = 5
 
 def convert_size(size_bytes):
     """Convert a number of bytes to a string with dynamic byte units"""
@@ -84,7 +87,8 @@ class protocol_sink(gr.basic_block):
     Radio flowgrpah, but it creates two named pipes and outputs data to them.
 
     """
-    def __init__(self, blocks_pipe, user_pipe, protocol_version, disable_api):
+    def __init__(self, blk_pipe, api_pipe, protocol_version, disable_api,
+                 disable_blk):
         gr.basic_block.__init__(self,
                                 name="protocol_sink",
                                 in_sig=[],
@@ -96,64 +100,201 @@ class protocol_sink(gr.basic_block):
         # Input parameters
         self.protocol_version = protocol_version
         self.disable_api      = disable_api
+        self.disable_blk      = disable_blk
 
         # Open named pipes
-        self.blocks_pipe      = Pipe(blocks_pipe)
+        if (not disable_blk):
+            self.blk_pipe = Pipe(blk_pipe)
 
         if ((self.protocol_version > 1) and (not disable_api)):
-            self.user_pipe = Pipe(user_pipe)
+            self.api_pipe = Pipe(api_pipe)
         else:
-            self.user_pipe = None
+            self.api_pipe = None
 
-        # Buffer to hold API data
+        # Buffer to accumulate incoming API data (from API Blocksat packets)
+        # until the entire user-transmitted data is accumulated
         self.api_buffer = b''
 
         # Stats
-        self.blocks_cnt   = 0
+        self.blk_data_cnt = 0
         self.api_data_cnt = 0
 
         # Print control
-        self.print_period = 10
-        self.next_print   = time.time() + 2*self.print_period
+        self.print_period            = 10
+        self.next_print              = time.time() + 2*self.print_period
+        self.last_api_warning        = 0
+        self.last_blk_warning        = 0
+        self.api_ovflw_print_pending = False
+        self.blk_ovflw_print_pending = False
+
+        # Initialize buffers to store output API and blocks packets
+        self.api_data_buffer          = list()
+        self.blk_data_buffer          = list()
+        self.api_data_buffer_data_cnt = 0
+        self.blk_data_buffer_data_cnt = 0
+
+        # Count the amount of data that has been deleted from the data buffer
+        # and not yet informed via a console log
+        self.api_data_buffer_del_cnt  = 0
+        self.blk_data_buffer_del_cnt  = 0
+
+        # Define threads to consume API and blocks data separately
+        self.api_consumer_en      = True
+        self.blk_consumer_en      = True
+        self.api_data_available   = Event()
+        self.blk_data_available   = Event()
+        self.api_data_buffer_lock = Lock()
+        self.blk_data_buffer_lock = Lock()
+        api_consumer              = Thread(target=self.api_consumer)
+        blk_consumer              = Thread(target=self.blk_consumer)
+        api_consumer.daemon       = True
+        blk_consumer.daemon       = True
+
+        # Start threads
+        if (not disable_api):
+            api_consumer.start()
+
+        if (not disable_blk):
+            blk_consumer.start()
 
         # Register the message port
         self.message_port_register_in(pmt.intern('async_pdu'))
         self.set_msg_handler(pmt.intern('async_pdu'), self.handle_msg)
 
-    def output_packet_data(self, packet):
-        """Output the content of a Blocksat Packet to named pipe(s)
+    def handle_pkt(self, packet):
+        """Process an incoming Blocksat Packet
         """
         if packet.pkt_type == TYPE_API_DATA and (not self.disable_api):
-            # Save data in buffer
+            # Accumulate data from the packet's payload in a buffer
             self.api_buffer += packet.payload
 
-            # Wait the last fragment and then output the data within a structure
+            # Wait until the last fragment and then save the data for output
             if (not packet.more_fragments):
-                # Struct has delimiter and message length
-                out_data_header = struct.pack(OUT_DATA_HEADER_FORMAT,
-                                              OUT_DATA_DELIMITER,
-                                              len(self.api_buffer))
+                # Read the API data buffer occupancy in the beginning
+                self.api_data_buffer_lock.acquire()
+                api_data_buffer_occ = len(self.api_data_buffer)
 
-                # Output to pipe
-                self.user_pipe.write(out_data_header + self.api_buffer)
+                # Save the entire buffer, now containing all of the data
+                # transmitted by an API user that was carried over one or more
+                # Blocksat packets
+                self.api_data_buffer.append(self.api_buffer)
+                self.api_data_buffer_data_cnt += len(self.api_buffer)
+                self.api_data_buffer_lock.release()
 
-                # And clean the API data buffer
+                # Warn about an imminent buffer overflow and in the case of
+                # overflow, throw away old data
+                buffer_occ_ratio = (float(self.api_data_buffer_data_cnt) /
+                                    MAX_BUFFER_SIZE) * 100
+                if (buffer_occ_ratio > OVERFLOW_WARNING_POINT and
+                    buffer_occ_ratio > (self.last_api_warning + 1) and
+                    buffer_occ_ratio < 100):
+                    print("%s API data buffer is %d%% full" %(
+                        "[" + time.strftime("%Y-%m-%d %H:%M:%S") + "]",
+                        buffer_occ_ratio
+                    ))
+                    print("Is your application running and connected to \"%s\"?"
+                          %(self.api_pipe.name))
+                    self.last_api_warning = buffer_occ_ratio
+
+                elif (buffer_occ_ratio >= 100):
+                    # Throw away old data until it fits
+                    self.api_data_buffer_lock.acquire()
+                    throw_away_cnt = 0
+                    while (self.api_data_buffer_data_cnt > MAX_BUFFER_SIZE):
+                        api_data = self.api_data_buffer.pop(0)
+                        throw_away_cnt += len(api_data)
+                        self.api_data_buffer_data_cnt -= len(api_data)
+
+                    self.api_data_buffer_lock.release()
+
+                    # Keep track of the overall amount of data deleted
+                    self.api_data_buffer_del_cnt += throw_away_cnt
+
+                    # Set to print buffer overflow message to console
+                    self.api_ovflw_print_pending = True
+
+                # Flag data availability if buffer was empty previously
+                if (api_data_buffer_occ == 0):
+                    self.api_data_available.set()
+
+                # Clean the API data buffer
                 self.api_buffer = b''
 
             self.api_data_cnt += len(packet.payload)
 
-        elif packet.pkt_type == TYPE_BLOCK:
-            self.blocks_pipe.write(packet.payload)
-            self.blocks_cnt += len(packet.payload)
+        elif packet.pkt_type == TYPE_BLOCK and (not self.disable_blk):
+            # Read the API data buffer occupancy and save the blocks data
+            self.blk_data_buffer_lock.acquire()
+            blk_data_buffer_occ = len(self.blk_data_buffer)
+            self.blk_data_buffer.append(packet.payload)
+            self.blk_data_buffer_data_cnt += len(packet.payload)
+            self.blk_data_buffer_lock.release()
+
+            # Warn about an imminent buffer overflow
+            buffer_occ_ratio = (float(self.blk_data_buffer_data_cnt) /
+                                MAX_BUFFER_SIZE) * 100
+            if (buffer_occ_ratio > OVERFLOW_WARNING_POINT and
+                buffer_occ_ratio > (self.last_blk_warning + 1) and
+                buffer_occ_ratio < 100):
+                print("%s Blocks data buffer is %d%% full" %(
+                    "[" + time.strftime("%Y-%m-%d %H:%M:%S") + "]",
+                    buffer_occ_ratio
+                ))
+                print("Is Bitcoin FIBRE running and listening to \"%s\"?"
+                      %(self.blk_pipe.name))
+                self.last_blk_warning = buffer_occ_ratio
+
+            elif (buffer_occ_ratio >= 100):
+                # Throw away old data until it fits
+                self.blk_data_buffer_lock.acquire()
+                throw_away_cnt = 0
+                while (self.blk_data_buffer_data_cnt > MAX_BUFFER_SIZE):
+                    blk_data = self.blk_data_buffer.pop(0)
+                    throw_away_cnt += len(blk_data)
+                    self.blk_data_buffer_data_cnt -= len(blk_data)
+
+                self.blk_data_buffer_lock.release()
+
+                # Keep track of the overall amount of data deleted
+                self.blk_data_buffer_del_cnt += throw_away_cnt
+
+                # Set to print buffer overflow message to console
+                self.blk_ovflw_print_pending = True
+
+            # Flag data availability if buffer was empty previously
+            if (blk_data_buffer_occ == 0):
+                self.blk_data_available.set()
+
+            self.blk_data_cnt += len(packet.payload)
 
         # Is it time to print data stats?
         current_t = time.time()
         if (current_t > self.next_print):
+            if self.api_ovflw_print_pending:
+                print("%s WARNING: API data buffer full - lost %d bytes" %(
+                    "[" + time.strftime("%Y-%m-%d %H:%M:%S") + "]",
+                    self.api_data_buffer_del_cnt
+                ))
+                print("NOTE: If not reading API data from \"%s\", "
+                      %(self.api_pipe.name) + "run with \"--no-api\"" )
+                self.api_data_buffer_del_cnt = 0
+                self.api_ovflw_print_pending = False
+
+            elif self.blk_ovflw_print_pending:
+                print("%s WARNING: Blocks data buffer full - lost %d bytes" %(
+                    "[" + time.strftime("%Y-%m-%d %H:%M:%S") + "]",
+                    self.blk_data_buffer_del_cnt
+                ))
+                print("NOTE: If not reading blocks data from \"%s\", "
+                      %(self.blk_pipe.name) + "run with \"--no-blocks\"" )
+                self.blk_data_buffer_del_cnt = 0
+                self.blk_ovflw_print_pending = False
+
             # Print data counts with timestamp
             timestamp  = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             print("--------------------------------------------------------------------------------")
             print('[%s] Rx Data\t Blocks: %7s\tUser API: %7s' %(
-                timestamp, convert_size(self.blocks_cnt),
+                timestamp, convert_size(self.blk_data_cnt),
                 convert_size(self.api_data_cnt)))
             print("--------------------------------------------------------------------------------")
 
@@ -161,10 +302,10 @@ class protocol_sink(gr.basic_block):
             self.next_print = current_t + self.print_period
 
     def handle_msg(self, msg_pmt):
-        """Processing incoming GNU Radio PDU with Blocksat PDU
+        """Process incoming GNU Radio PDU containing a Blocksat Packet
 
         Args:
-            msg_pmt : the message containing the Blocksat PDU
+            msg_pmt : the message containing a Blocksat Packet
         """
 
         # Get metadata and information from message
@@ -179,14 +320,74 @@ class protocol_sink(gr.basic_block):
         # Convert incoming packet to a bytes string
         raw_pkt = b''.join([chr(x) for x in pmt.u8vector_elements(msg)])
 
-        # Read the
         if (self.protocol_version > 1):
             # Parse Blocksat Packet
             packet = BlocksatPacket(raw_pkt)
 
-            # Output packet content
-            self.output_packet_data(packet)
+            # Process the parsed packet
+            self.handle_pkt(packet)
         else:
             # Write raw data directly to named pipe
-            self.blocks_pipe.write(raw_pkt)
+            self.blk_pipe.write(raw_pkt)
+
+    def api_consumer(self):
+        """Consumes data from the API data buffer and outputs to pipe
+        """
+        api_data_buffer_occ = 0
+
+        while self.api_consumer_en:
+            # Wait for data if the buffer is empty
+            if (api_data_buffer_occ == 0):
+                self.api_data_available.wait()
+                self.api_data_available.clear()
+
+            self.api_data_buffer_lock.acquire()
+
+            # Pop data
+            api_data = self.api_data_buffer.pop(0)
+
+            # Keep track of data count in bytes
+            self.api_data_buffer_data_cnt -= len(api_data)
+
+            # Keep track of the occupancy in terms of data blobs
+            api_data_buffer_occ = len(self.api_data_buffer)
+
+            self.api_data_buffer_lock.release()
+
+            # Prepare output struct header w/ delimiter and length
+            out_data_header = struct.pack(OUT_DATA_HEADER_FORMAT,
+                                          OUT_DATA_DELIMITER,
+                                          len(api_data))
+
+            # Output structure to pipe
+            self.api_pipe.write(out_data_header + api_data)
+
+    def blk_consumer(self):
+        """Consumes data from the blocks data buffer and outputs to pipe
+        """
+
+        blk_data_buffer_occ = 0
+
+        while self.blk_consumer_en:
+            # Wait for data if the buffer is empty
+            if (blk_data_buffer_occ == 0):
+                self.blk_data_available.wait()
+                self.blk_data_available.clear()
+
+            self.blk_data_buffer_lock.acquire()
+
+            # Pop data
+            blk_data = self.blk_data_buffer.pop(0)
+
+            # Keep track of data count in bytes
+            self.blk_data_buffer_data_cnt -= len(blk_data)
+
+            # Keep track of the occupancy in terms of data blobs
+            blk_data_buffer_occ = len(self.blk_data_buffer)
+
+            self.blk_data_buffer_lock.release()
+
+            # Output blocks data to pipe
+            # NOTE: output data structure is only used for API data
+            self.blk_pipe.write(blk_data)
 
