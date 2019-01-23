0680d18be74df86785fac05c7c4a41e8b08e1f89
Blockstream Satellite <satellite@blockstream.com>
2019-01-23 14:34:10 -0300

    Cosmetic fixes within turbo decoder wrappers
    Fix white spaces, indentation and alignment.

--- lib/turbo_decoder_impl.cc
+++ lib/turbo_decoder_impl.cc
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -28,27 +28,27 @@
 #undef _DEBUG
 
 namespace gr {
-  namespace blocksat {
+	namespace blocksat {
 
-    turbo_decoder::sptr
-    turbo_decoder::make(int N, int K)
-    {
-      return gnuradio::get_initial_sptr
-        (new turbo_decoder_impl(N, K));
-    }
+		turbo_decoder::sptr
+		turbo_decoder::make(int N, int K)
+		{
+			return gnuradio::get_initial_sptr
+				(new turbo_decoder_impl(N, K));
+		}
 
-    /*
-     * The private constructor
-     */
-    turbo_decoder_impl::turbo_decoder_impl(int N, int K)
-      : gr::block("turbo_decoder",
-              gr::io_signature::make(1, 1, sizeof(signed char)),
-              gr::io_signature::make(1, 1, sizeof(unsigned char)))
-    {
+		/*
+		 * The private constructor
+		 */
+		turbo_decoder_impl::turbo_decoder_impl(int N, int K)
+			: gr::block("turbo_decoder",
+			            gr::io_signature::make(1, 1, sizeof(signed char)),
+			            gr::io_signature::make(1, 1, sizeof(unsigned char)))
+		{
 
 			std::vector<int> poly = {013, 015};
 			int tail_length = (int)(2 * std::floor(std::log2((float)std::max(poly[0], poly[1]))));
-  		int N_cw        = 2 * K + tail_length;
+			int N_cw        = 2 * K + tail_length;
 
 			interleaver = new module::Interleaver_LTE<int> (K);
 			interleaver->init();
@@ -63,67 +63,67 @@ namespace gr {
 			printf("\n");
 #endif
 
-    	sub_dec = new module::Decoder_RSC_BCJR_seq_very_fast <B_8,Q_8,QD_8,tools::max<Q_8>,tools::max<QD_8>> (K, trellis);
+			sub_dec = new module::Decoder_RSC_BCJR_seq_very_fast <B_8,Q_8,QD_8,tools::max<Q_8>,tools::max<QD_8>> (K, trellis);
 
-    	dec = new module::Decoder_turbo_fast<B_8,Q_8>(K, N, 6, *interleaver, *sub_dec, *sub_dec);
+			dec = new module::Decoder_turbo_fast<B_8,Q_8>(K, N, 6, *interleaver, *sub_dec, *sub_dec);
 
-      set_fixed_rate(true);
-      set_relative_rate((double)K/(double)N);
-      set_output_multiple(K);
+			set_fixed_rate(true);
+			set_relative_rate((double)K/(double)N);
+			set_output_multiple(K);
 
-      d_input_size  = N * sizeof(signed char);
-      d_output_size = K * sizeof(signed char);
+			d_input_size  = N * sizeof(signed char);
+			d_output_size = K * sizeof(signed char);
 		}
 
-    /*
-     * Our virtual destructor.
-     */
-    turbo_decoder_impl::~turbo_decoder_impl()
-    {
-    }
+		/*
+		 * Our virtual destructor.
+		 */
+		turbo_decoder_impl::~turbo_decoder_impl()
+		{
+		}
 
-    int
-    turbo_decoder_impl::fixed_rate_ninput_to_noutput(int ninput)
-    {
+		int
+		turbo_decoder_impl::fixed_rate_ninput_to_noutput(int ninput)
+		{
 #ifdef _DEBUG
 			printf("[dec] input to output: %f\n", 0.5 + ninput*relative_rate());
 #endif
-      return (int)(0.5 + ninput*relative_rate());
-    }   
+			return (int)(0.5 + ninput*relative_rate());
+		}
 
-    int 
-    turbo_decoder_impl::fixed_rate_noutput_to_ninput(int noutput)
-    {   
+		int
+		turbo_decoder_impl::fixed_rate_noutput_to_ninput(int noutput)
+		{
 #ifdef _DEBUG
 			printf("[dec] output to input: %f\n", 0.5 + noutput/relative_rate());
 #endif
-      return (int)(0.5 + noutput/relative_rate());
-    }   
+			return (int)(0.5 + noutput/relative_rate());
+		}
 
-    void
-    turbo_decoder_impl::forecast(int noutput_items,
-                           gr_vector_int& ninput_items_required)
-    {   
-      ninput_items_required[0] = fixed_rate_noutput_to_ninput(noutput_items);
+		void
+		turbo_decoder_impl::forecast(int noutput_items,
+		                             gr_vector_int& ninput_items_required)
+		{
+			ninput_items_required[0] = fixed_rate_noutput_to_ninput(noutput_items);
 #ifdef _DEBUG
 			printf("[dec] Forecast %d %d\n", noutput_items, ninput_items_required[0]);
 #endif
-    }   
+		}
 
-    int 
-    turbo_decoder_impl::general_work(int noutput_items,
-                               gr_vector_int& ninput_items,
-                               gr_vector_const_void_star &input_items,
-                               gr_vector_void_star &output_items)
-    {   
+		int
+		turbo_decoder_impl::general_work(int noutput_items,
+		                                 gr_vector_int& ninput_items,
+		                                 gr_vector_const_void_star &input_items,
+		                                 gr_vector_void_star &output_items)
+		{
 
-      signed char *inbuffer  = (signed char*)input_items[0];
-      signed char *outbuffer = (signed char*)output_items[0];
+			signed char *inbuffer  = (signed char*)input_items[0];
+			signed char *outbuffer = (signed char*)output_items[0];
 #ifdef _DEBUG
 			printf("[dec] in[%d] out[%d]\n", ninput_items[0], noutput_items);
 #endif
 
-      for(int i = 0; i < noutput_items/output_multiple(); i++) {
+			for(int i = 0; i < noutput_items/output_multiple(); i++) {
 				dec->_decode_siho(inbuffer+(i*d_input_size), outbuffer+(i*d_output_size), 0);
 
 #ifdef _DEBUG
@@ -137,12 +137,12 @@ namespace gr {
 				printf("\n");
 #endif
 
-      }   
+			}
 
-      consume_each(fixed_rate_noutput_to_ninput(noutput_items));
-      return noutput_items;
-    }
+			consume_each(fixed_rate_noutput_to_ninput(noutput_items));
+			return noutput_items;
+		}
 
-  } /* namespace blocksat */
+	} /* namespace blocksat */
 } /* namespace gr */
 
--- lib/turbo_decoder_impl.h
+++ lib/turbo_decoder_impl.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -37,40 +37,40 @@
 using namespace aff3ct;
 
 namespace gr {
-  namespace blocksat {
+	namespace blocksat {
 
-    class turbo_decoder_impl : public turbo_decoder
-    {
-     private:
-      // Nothing to declare in this block.
+		class turbo_decoder_impl : public turbo_decoder
+		{
+		private:
+			// Nothing to declare in this block.
 			module::Encoder_RSC_generic_sys<B_8> *sub_enc;
-  		module::Decoder_RSC_BCJR_seq_very_fast
-				<B_8,Q_8,QD_8,tools::max<Q_8>,tools::max<QD_8>> * sub_dec;
+			module::Decoder_RSC_BCJR_seq_very_fast
+			<B_8,Q_8,QD_8,tools::max<Q_8>,tools::max<QD_8>> * sub_dec;
 			module::Interleaver_LTE<int> *interleaver;
-  		module::Decoder_turbo_fast<B_8,Q_8> *dec;
-      int d_input_size  ;
-      int d_output_size ;
+			module::Decoder_turbo_fast<B_8,Q_8> *dec;
+			int d_input_size  ;
+			int d_output_size ;
 
-     public:
-      turbo_decoder_impl(int N, int K);
-      ~turbo_decoder_impl();
+		public:
+			turbo_decoder_impl(int N, int K);
+			~turbo_decoder_impl();
 
-      // Where all the action really happens
-      //int work(int noutput_items,
-      //   gr_vector_const_void_star &input_items,
-      //   gr_vector_void_star &output_items);
-      int general_work(int noutput_items,
-                       gr_vector_int& ninput_items,
-                       gr_vector_const_void_star &input_items,
-                       gr_vector_void_star &output_items);
-      int fixed_rate_ninput_to_noutput(int ninput);
-      int fixed_rate_noutput_to_ninput(int noutput);
-      void forecast(int noutput_items,
-                    gr_vector_int& ninput_items_required);
+			// Where all the action really happens
+			//int work(int noutput_items,
+			//   gr_vector_const_void_star &input_items,
+			//   gr_vector_void_star &output_items);
+			int general_work(int noutput_items,
+			                 gr_vector_int& ninput_items,
+			                 gr_vector_const_void_star &input_items,
+			                 gr_vector_void_star &output_items);
+			int fixed_rate_ninput_to_noutput(int ninput);
+			int fixed_rate_noutput_to_ninput(int noutput);
+			void forecast(int noutput_items,
+			              gr_vector_int& ninput_items_required);
 
-    };
+		};
 
-  } // namespace blocksat
+	} // namespace blocksat
 } // namespace gr
 
 #endif /* INCLUDED_BLOCKSAT_TURBO_DECODER_IMPL_H */
